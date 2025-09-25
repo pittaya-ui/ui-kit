5251ecaf517799d81e48aa2c9884f00375279d8a
marcosvbueno <mvbueno07@gmail.com>
2025-09-25 09:32:07 -0300

    feat: update homepage layout and content for improved clarity and aesthetics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,14 @@ export default function Home() {
   return (
     <>
       <PittayaBackground className="h-screen overflow-x-hidden">
-        <main className="flex min-h-screen flex-1 items-center justify-center px-6">
+        <main className="flex flex-col min-h-screen flex-1 items-center justify-center px-6 gap-10">
           <div className="max-w-4xl space-y-8 text-center">
-            <h1 className="text-6xl leading-tight font-bold text-white">
-              Bem-vindo ao <br />
-              <span className="text-white">Pittaya UI</span>
+            <h1 className="text-6xl leading-tight font-bold text-white  [text-shadow:0_0_20px_rgba(255,255,255,0.6)]">
+              Components that scale <br />
+              <span className="text-white">with your ideas.</span>
             </h1>
-            <p className="mx-auto max-w-2xl text-xl text-white/80">
-              Componentes modernos e animações incríveis para suas aplicações
-              Next.js. Construa interfaces bonitas com facilidade.
+            <p className="mx-auto max-w-2xl text-xl text-white/90">
+              A fully open-source UI library for React, powered by TypeScript and Tailwind CSS.Fast, composable, and ready for production.
             </p>
           </div>
         </main>
