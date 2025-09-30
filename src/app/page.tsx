ebf86df8f9da2f995583ae968f8657577c941069
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-09-30 10:23:49 -0300

    refactor: enhance layout structure and improve page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,19 @@ import { PittayaBackground } from "@/components/raycast-animated-background";
 
 export default function Home() {
   return (
-    <>
-      <PittayaBackground className="h-screen overflow-x-hidden">
-        <main className="flex flex-col min-h-screen flex-1 items-center justify-center px-6 gap-10">
-          <div className="max-w-4xl space-y-8 text-center">
-            <h1 className="text-6xl leading-tight font-bold text-white  [text-shadow:0_0_20px_rgba(255,255,255,0.6)]">
-              Components that scale <br />
-              <span className="text-white">with your ideas.</span>
-            </h1>
-            <p className="mx-auto max-w-2xl text-xl text-white/90">
-              A fully open-source UI library for React, powered by TypeScript and Tailwind CSS.Fast, composable, and ready for production.
-            </p>
-          </div>
-        </main>
-      </PittayaBackground>
-    </>
+    <PittayaBackground className="h-screen max-h-screen overflow-x-hidden overflow-y-hidden">
+      <main className="flex h-screen flex-1 flex-col items-center justify-center gap-10 px-6">
+        <div className="max-w-4xl space-y-8 text-center">
+          <h1 className="text-6xl leading-tight font-bold text-white [text-shadow:0_0_20px_rgba(255,255,255,0.6)]">
+            Components that scale <br />
+            <span className="text-white">with your ideas.</span>
+          </h1>
+          <p className="mx-auto max-w-2xl text-xl text-white/90">
+            A fully open-source UI library for React, powered by TypeScript and
+            Tailwind CSS.Fast, composable, and ready for production.
+          </p>
+        </div>
+      </main>
+    </PittayaBackground>
   );
 }
