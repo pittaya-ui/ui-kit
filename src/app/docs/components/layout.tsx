ffe1058d7f86c229a98a19e807f6571bc2aef20a
marcosvbueno <mvbueno07@gmail.com>
2025-11-11 11:03:35 -0300

    feat: add new layout and components docs/components pages

---
+++ src/app/docs/components/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function ComponentLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="mx-auto flex w-full max-w-7xl gap-6 pt-24 pb-16 lg:grid lg:grid-cols-[260px_minmax(0,1fr)] xl:grid-cols-[260px_minmax(0,1fr)_220px]">
+      {children}
+    </div>
+  );
+}
